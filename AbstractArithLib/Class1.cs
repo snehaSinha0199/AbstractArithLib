0564e15c9827c40ae9c1b74c262a8d1b774e9fc0
sneha.sinha3 <sneha.sinha3@NAG1-LHP-N76248>
2022-08-08 13:47:35 +0530

    Add project files.

---
+++ AbstractArithLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace AbstractArithLib
+{
+    /*
+     * 1. ArithmeticOperations class - instance class with 4 methods
+2. Declare interface including 4 methods and implement in your class
+3. Declare interfaces for each methods and implement all the interfaces in your derived class
+4. Declare Abstract class, implement it in derived class
+5. console app - instace class output, interface object output, multiple interfaces object output, abstract output
+*/
+    public class Class1
+    {
+
+    }
+}
